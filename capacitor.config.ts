3d1b8199d6a3643cd1be6bf81eddd8d72e8a53fe
Killuasito <tififerreira@gmail.com>
2026-04-08 11:05:39 -0300

    Atualização de Logo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.delivery.app',
-  appName: 'App-Delivery',
+  appName: 'App Delivery',
   webDir: 'out'
 };
 
